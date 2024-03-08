becd8dc33b68b50d64a4d7c6a0f79988db31af58
gerardo linares <geracoder@gmail.com>
2024-03-08 14:49:12 -0300

    Initial setup and layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,29 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1200px",
+    },
+    extend: {
+      colors: {
+        primary: "#131424",
+        secondary: "#393A47",
+        accent: "#F13024",
+      },
+      backgroundImage: {
+        site: 'url("/site-bg.svg")',
+      },
+      animation: {
+        "spin-slow": "spin 6s linear infinite",
+      },
+      fontFamily: {
+        poppins: [`var(--font-poppins)`, "sans-serif"],
+        sora: [`var(--font-sora)`, "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
